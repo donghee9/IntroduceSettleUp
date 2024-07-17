7cd07bea94e665824b5824b1779b06c9f7421e0f
seodonghee 서동희 <129722492+donghee9@users.noreply.github.com>
2024-07-18 08:27:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const githubBtn = document.getElementById('github-btn');
+    const howToMakeBtn = document.getElementById('how-to-make-btn');
+    const contentContainer = document.getElementById('content-container');
+    const languageSelection = document.getElementById('language-selection');
+
+    githubBtn.addEventListener('click', () => {
+        window.location.href = 'https://github.com/Settle-Up/settle-up-server';
+    });
+
+    howToMakeBtn.addEventListener('click', () => {
+        contentContainer.classList.add('active');
+        howToMakeBtn.classList.add('active');
+        languageSelection.classList.remove('center');
+        languageSelection.classList.add('top-left');
+    });
+});
 
