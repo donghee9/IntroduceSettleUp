cd9595bd5388776be2b8fe36a3883df639256ab3
seodonghee 서동희 <129722492+donghee9@users.noreply.github.com>
2024-07-18 12:13:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const howToMakeBtn = document.getElementById('how-to-make-btn');
     const contentContainer = document.getElementById('content-container');
     const languageSelection = document.getElementById('language-selection');
+    const reportIssueBtn = document.getElementById('report-issue-btn');
+    const reportIssueContainer = document.getElementById('report-issue-container');
+    const closeButton = document.querySelector('.close-button');
+    const reportIssueForm = document.getElementById('report-issue-form');
+    const feedbackMessage = document.getElementById('feedback-message');
 
     githubBtn.addEventListener('click', () => {
         window.location.href = 'https://github.com/Settle-Up/settle-up-server';
@@ -13,5 +18,35 @@ document.addEventListener('DOMContentLoaded', () => {
         howToMakeBtn.classList.add('active');
         languageSelection.classList.remove('center');
         languageSelection.classList.add('above-invoice'); // Position buttons above the invoice
+    });
+
+    reportIssueBtn.addEventListener('click', () => {
+        reportIssueContainer.style.display = 'flex';
+    });
+
+    closeButton.addEventListener('click', () => {
+        reportIssueContainer.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === reportIssueContainer) {
+            reportIssueContainer.style.display = 'none';
+        }
+    });
+
+    reportIssueForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const issueLocation = document.getElementById('issue-location').value;
+        const issueDescription = document.getElementById('issue-description').value;
+        
+        const mailtoLink = `mailto:seodonghee456@gmail.com?subject=Issue Report&body=Where did the issue occur?: ${issueLocation}%0D%0A%0D%0AHow would you like it to be improved?: ${issueDescription}`;
+        
+        window.location.href = mailtoLink;
+
+        reportIssueContainer.style.display = 'none';
+        feedbackMessage.style.display = 'block';
+        setTimeout(() => {
+            feedbackMessage.style.display = 'none';
+        }, 5000);
     });
 });
